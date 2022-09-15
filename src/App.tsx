c1e452a8b4bc64a5520f3ab9006ab3e8a3b2e867
HenrikEide <Henrik225@gmail.com>
2022-09-15 21:44:19 +0200

    now with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from "react";
+import Navbar from "./Navbar";
 
 export default function App() {
   return (
-    <div>
-      <nav>E</nav>
-      <div>
-        <h2>Steike</h2>
+    <>
+      <Navbar />
+      <div className="min-h-full">
+        <header className="bg-white shadow">
+          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+              Homepage
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            {/* Replace with your content */}
+            <div className="px-4 py-6 sm:px-0">
+              <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+            </div>
+            {/* /End replace */}
+          </div>
+        </main>
       </div>
-    </div>
+    </>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
