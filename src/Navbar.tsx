b4923fadf57fc6672742f15c8defbb40a3f3dafd
HenrikEide <Henrik225@gmail.com>
2022-09-25 23:59:23 +0200

    Navbar real goofy

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -16,18 +16,34 @@ export default function Navbar() {
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 items-center justify-between">
-              <div className="flex items-center">
-                <div className="flex-shrink-0">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
                   <img
-                    className="h-8 w-8"
-                    src="https://www.clipartmax.com/png/middle/27-274923_coffee-cup-cappuccino-cafe-hot-coffee-vector-png.png"
-                    alt="Some logo"
+                    className="block h-8 w-auto lg:hidden"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  />
+                  <img
+                    className="hidden h-8 w-auto lg:block"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
                   />
                 </div>
-                <div className="hidden md:block">
-                  <div className="ml-10 flex items-baseline space-x-4">
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
@@ -46,21 +62,12 @@ export default function Navbar() {
                   </div>
                 </div>
               </div>
-              <div className="-mr-2 flex md:hidden">
-                {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div>
             </div>
           </div>
-          <Disclosure.Panel className="md:hidden">
-            <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
@@ -82,5 +89,74 @@ export default function Navbar() {
         </>
       )}
     </Disclosure>
+    // <Disclosure as="nav" className="bg-gray-800">
+    //   {({ open }) => (
+    //     <>
+    //       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    //         <div className="flex h-16 items-center justify-between">
+    //           <div className="flex items-center">
+    //             <div className="flex-shrink-0">
+    //               <img
+    //                 className="h-8 w-8"
+    //                 src="https://www.clipartmax.com/png/middle/27-274923_coffee-cup-cappuccino-cafe-hot-coffee-vector-png.png"
+    //                 alt="Some logo"
+    //               />
+    //             </div>
+    //             <div className="hidden md:block">
+    //               <div className="ml-10 flex items-baseline space-x-4">
+    //                 {navigation.map((item) => (
+    //                   <a
+    //                     key={item.name}
+    //                     href={item.href}
+    //                     className={classNames(
+    //                       item.current
+    //                         ? "bg-gray-900 text-white"
+    //                         : "text-gray-300 hover:bg-gray-700 hover:text-white",
+    //                       "px-3 py-2 rounded-md text-sm font-medium"
+    //                     )}
+    //                     aria-current={item.current ? "page" : undefined}
+    //                   >
+    //                     {item.name}
+    //                   </a>
+    //                 ))}
+    //               </div>
+    //             </div>
+    //           </div>
+    //           <div className="-mr-2 flex md:hidden">
+    //             {/* Mobile menu button */}
+    //             <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+    //               <span className="sr-only">Open main menu</span>
+    //               {open ? (
+    //                 <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+    //               ) : (
+    //                 <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+    //               )}
+    //             </Disclosure.Button>
+    //           </div>
+    //         </div>
+    //       </div>
+    //       <Disclosure.Panel className="md:hidden">
+    //         <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+    //           {navigation.map((item) => (
+    //             <Disclosure.Button
+    //               key={item.name}
+    //               as="a"
+    //               href={item.href}
+    //               className={classNames(
+    //                 item.current
+    //                   ? "bg-gray-900 text-white"
+    //                   : "text-gray-300 hover:bg-gray-700 hover:text-white",
+    //                 "block px-3 py-2 rounded-md text-base font-medium"
+    //               )}
+    //               aria-current={item.current ? "page" : undefined}
+    //             >
+    //               {item.name}
+    //             </Disclosure.Button>
+    //           ))}
+    //         </div>
+    //       </Disclosure.Panel>
+    //     </>
+    //   )}
+    // </Disclosure>
   );
 }
