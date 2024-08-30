f3f116050b5a865ab531a7101eab306359b25532
HenrikEide <Henrik225@gmail.com>
2024-08-30 15:11:51 +0200

    lil larger

--- src/CardL.tsx
+++ src/CardL.tsx
@@ -7,7 +7,7 @@ export default function CardL() {
         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12 sm:max-w-md">
           {/* Embed website */}
           <embed src="https://tavla.entur.no/y2moNW6V6SRKftFunSFA" 
-            width={Math.min(window.innerWidth, 700)} height={Math.min(window.innerHeight - 200, 1000)}/>
+            width={Math.min(window.innerWidth, 700)} height={Math.min(window.innerHeight - 100, 1000)}/>
         </div>
       </div>
     </div>
